79807d1bb59f0ac8360331162fa9ce94f374aff9
Roshan Thakur <86141807+roshanofficial@users.noreply.github.com>
2022-03-30 19:35:49 +0545

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,54 @@
+new WOW().init();
+
+
+        // mixitup 
+        $(document).ready(function () {
+            var mixier = mixitup(".portfolio_item");
+        });
+        var typed = new Typed('.animationText', {
+          strings: [
+              "Developer",
+              "programmer",
+              "designer"
+          ],
+          typeSpeed:80,
+          backSpeed:80,
+          loop:true
+        });
+
+        $(".counter_num").counterUp({
+            delay: 10,
+            time:1000
+        })
+
+        $(window).scroll(function(){
+            if($(this).scrollTop()>200){
+                $(".scroll_class").addClass("strickyy") 
+            }
+            else{
+                $(".scroll_class").removeClass("strickyy")
+            }
+        })
+        $(window).scroll(function(){
+            if($(this).scrollTop()>250){
+                
+                $(".scroll_box").show();
+            }
+            else{
+                $(".scroll_box").hide();  
+            }
+        })
+        setTimeout(()=> {
+            $("#loader").hide()
+            $("#main").show()
+        },3000)
+
+var preloader = document.getElementById('loading')
+
+
+function my_function(){
+    // setTimeout(() => {
+        preloader.style.display='none';
+    // }, 2000); 
+}
+ 
